288095cbdc136a5a1c8da3c37d571a2398fc47cb
Mattias Edlund <matte.edlund@gmail.com>
2018-06-10 01:29:03 +0900

    Created a new class for active torrents.

---
+++ TorrentSwifter/Torrent.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using TorrentSwifter.Helpers;
+
+namespace TorrentSwifter
+{
+    /// <summary>
+    /// A torrent.
+    /// </summary>
+    public sealed class Torrent
+    {
+        #region Consts
+        private const int MinBlockSize = 1 * (int)SizeHelper.KiloByte;
+        private const int MaxBlockSize = 16 * (int)SizeHelper.KiloByte;
+        private const int DefaultBlockSize = 16 * (int)SizeHelper.KiloByte;
+        #endregion
+
+        #region Fields
+        private readonly TorrentMetaData metaData;
+        private readonly string downloadPath;
+        private readonly int blockSize;
+        private readonly long totalSize;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets the torrent meta-data information.
+        /// </summary>
+        public TorrentMetaData MetaData
+        {
+            get { return metaData; }
+        }
+
+        /// <summary>
+        /// Gets the path to download to.
+        /// </summary>
+        public string DownloadPath
+        {
+            get { return downloadPath; }
+        }
+
+        /// <summary>
+        /// Gets the size of each piece.
+        /// </summary>
+        public int PieceSize
+        {
+            get { return metaData.PieceSize; }
+        }
+
+        /// <summary>
+        /// Gets the size of each block.
+        /// </summary>
+        public int BlockSize
+        {
+            get { return blockSize; }
+        }
+
+        /// <summary>
+        /// Gets the total size of this torrent.
+        /// </summary>
+        public long TotalSize
+        {
+            get { return totalSize; }
+        }
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Creates a new torrent.
+        /// </summary>
+        /// <param name="metaData">The torrent meta-data information.</param>
+        /// <param name="downloadPath">The download path for the torrent.</param>
+        /// <param name="blockSize">The desired block size.</param>
+        public Torrent(TorrentMetaData metaData, string downloadPath, int blockSize = DefaultBlockSize)
+        {
+            if (metaData == null)
+                throw new ArgumentNullException("metaData");
+            else if (blockSize < MinBlockSize)
+                throw new ArgumentOutOfRangeException("blockSize", string.Format("The block size must be above or equals to {0} bytes ({1}).", MinBlockSize, SizeHelper.GetHumanReadableSize(MinBlockSize)));
+            else if (blockSize > MaxBlockSize)
+                throw new ArgumentOutOfRangeException("blockSize", string.Format("The block size must be below or equals to {0} bytes ({1}).", MaxBlockSize, SizeHelper.GetHumanReadableSize(MaxBlockSize)));
+            else if (!MathHelper.IsPowerOfTwo(blockSize))
+                throw new ArgumentException("The block size must be a power of two.", "blockSize");
+
+            this.metaData = metaData;
+            this.downloadPath = Path.GetFullPath(downloadPath);
+            this.blockSize = blockSize;
+            this.totalSize = metaData.TotalSize;
+        }
+        #endregion
+    }
+}
