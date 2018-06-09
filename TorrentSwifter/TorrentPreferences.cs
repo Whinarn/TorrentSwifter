19645d269023e98c61e31805391553b0aa2b6f4a
Mattias Edlund <matte.edlund@gmail.com>
2018-06-10 01:27:16 +0900

    Created a new class for torrent preferences.

---
+++ TorrentSwifter/TorrentPreferences.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TorrentSwifter
+{
+    /// <summary>
+    /// Torrent preferences.
+    /// </summary>
+    public static class TorrentPreferences
+    {
+        #region Fields
+        private static bool allocateFullFileSizes = false;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets or sets if the full file sizes are allocated for new torrent downloads.
+        /// </summary>
+        public static bool AllocateFullFileSizes
+        {
+            get { return allocateFullFileSizes; }
+            set { allocateFullFileSizes = value; }
+        }
+        #endregion
+    }
+}
