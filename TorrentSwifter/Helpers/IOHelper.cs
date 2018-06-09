812e5547e424c23360eb351de338e56a72ddc6cf
Mattias Edlund <matte.edlund@gmail.com>
2018-06-09 22:48:17 +0900

    Created a class for I/O helper methods.

---
+++ TorrentSwifter/Helpers/IOHelper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+
+namespace TorrentSwifter.Helpers
+{
+    internal static class IOHelper
+    {
+        public static string GetParentDirectory(string filePath)
+        {
+            return Path.GetDirectoryName(filePath);
+        }
+
+        public static void CreateDirectoryIfItDoesntExist(string directoryPath)
+        {
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+        }
+
+        public static void CreateParentDirectoryIfItDoesntExist(string filePath)
+        {
+            string parentDirectoryPath = GetParentDirectory(filePath);
+            CreateDirectoryIfItDoesntExist(parentDirectoryPath);
+        }
+
+        public static string GetLocalPath(string fullPath, string rootPath)
+        {
+            fullPath = Path.GetFullPath(fullPath);
+            rootPath = Path.GetFullPath(rootPath);
+
+            if (Path.DirectorySeparatorChar != '/')
+            {
+                fullPath = fullPath.Replace(Path.DirectorySeparatorChar, '/');
+                rootPath = rootPath.Replace(Path.DirectorySeparatorChar, '/');
+            }
+            if (Path.AltDirectorySeparatorChar != '/')
+            {
+                fullPath = fullPath.Replace(Path.AltDirectorySeparatorChar, '/');
+                rootPath = rootPath.Replace(Path.AltDirectorySeparatorChar, '/');
+            }
+
+            // Add the ending slash if it's not present
+            if (!rootPath.EndsWith("/"))
+            {
+                rootPath += "/";
+            }
+
+            if (!fullPath.StartsWith(rootPath, StringComparison.OrdinalIgnoreCase))
+                throw new InvalidOperationException(string.Format("The path '{0}' is not a part of the directory '{1}'.", fullPath, rootPath));
+
+            string localPath = fullPath.Substring(rootPath.Length);
+            return localPath;
+        }
+
+        public static string GetTorrentFilePath(string rootPath, TorrentMetaData.FileItem fileItem)
+        {
+            string fileLocalPath = fileItem.Path;
+            if (fileLocalPath == null)
+                throw new InvalidOperationException("The file does not have a path.");
+
+            if (Path.DirectorySeparatorChar != '/')
+            {
+                fileLocalPath = fileLocalPath.Replace('/', Path.DirectorySeparatorChar);
+            }
+
+            string fileFullPath = Path.Combine(rootPath, fileLocalPath);
+            fileFullPath = Path.GetFullPath(fileFullPath);
+            return fileFullPath;
+        }
+    }
+}
