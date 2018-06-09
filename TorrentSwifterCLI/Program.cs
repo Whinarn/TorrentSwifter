056ec6e7d89392e37dccafefb6c9b922ea97b8af
Mattias Edlund <matte.edlund@gmail.com>
2018-06-09 19:48:28 +0900

    Created a base project for a CLI using the TorrentSwifter library.

---
+++ TorrentSwifterCLI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TorrentSwifter;
+
+namespace TorrentSwifterCLI
+{
+    class Program
+    {
+        const string DefaultTorrentPath = @"C:\Users\Whinarn\Downloads\Game.Of.Thrones.S01-S05.1080p.BluRay.x264-Scene [NO RAR].torrent";
+
+        static void Main(string[] args)
+        {
+            string torrentFilePath = (args.Length > 0 ? args[0] : DefaultTorrentPath);
+
+            var torrentInfo = new TorrentInfo();
+            torrentInfo.LoadFromFile(torrentFilePath);
+
+            Console.WriteLine("Info Hash: {0}", torrentInfo.InfoHash);
+        }
+    }
+}
