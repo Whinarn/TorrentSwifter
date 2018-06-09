52474fa93ab41e66e7bb2e12434c57f43fdd8c50
Mattias Edlund <matte.edlund@gmail.com>
2018-06-09 21:44:15 +0900

    The hash helper is now thread-safe.

--- TorrentSwifter/Helpers/HashHelper.cs
+++ TorrentSwifter/Helpers/HashHelper.cs
@@ -1,57 +1,58 @@
 ﻿using System;
 using System.IO;
 using System.Security.Cryptography;
+using System.Threading;
 
 namespace TorrentSwifter.Helpers
 {
     internal static class HashHelper
     {
-        private static readonly MD5 md5 = MD5.Create();
-        private static readonly SHA1 sha1 = SHA1.Create();
+        private static readonly ThreadLocal<MD5> md5 = new ThreadLocal<MD5>(() => MD5.Create());
+        private static readonly ThreadLocal<SHA1> sha1 = new ThreadLocal<SHA1>(() => SHA1.Create());
 
         public static byte[] ComputeMD5(byte[] data)
         {
-            return md5.ComputeHash(data);
+            return md5.Value.ComputeHash(data);
         }
 
         public static byte[] ComputeMD5(byte[] data, int offset, int count)
         {
-            return md5.ComputeHash(data, offset, count);
+            return md5.Value.ComputeHash(data, offset, count);
         }
 
         public static byte[] ComputeMD5(Stream stream)
         {
-            return md5.ComputeHash(stream);
+            return md5.Value.ComputeHash(stream);
         }
 
         public static byte[] ComputeFileMD5(string filePath)
         {
             using (var stream = File.OpenRead(filePath))
             {
-                return md5.ComputeHash(stream);
+                return md5.Value.ComputeHash(stream);
             }
         }
 
         public static byte[] ComputeSHA1(byte[] data)
         {
-            return sha1.ComputeHash(data);
+            return sha1.Value.ComputeHash(data);
         }
 
         public static byte[] ComputeSHA1(byte[] data, int offset, int count)
         {
-            return sha1.ComputeHash(data, offset, count);
+            return sha1.Value.ComputeHash(data, offset, count);
         }
 
         public static byte[] ComputeSHA1(Stream stream)
         {
-            return sha1.ComputeHash(stream);
+            return sha1.Value.ComputeHash(stream);
         }
 
         public static byte[] ComputeFileSHA1(string filePath)
         {
             using (var stream = File.OpenRead(filePath))
             {
-                return sha1.ComputeHash(stream);
+                return sha1.Value.ComputeHash(stream);
             }
         }
     }
