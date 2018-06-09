6120b85ee881570268a4294e9a41981169b105e5
Mattias Edlund <matte.edlund@gmail.com>
2018-06-10 01:32:28 +0900

    Added file initialization to torrents.

--- TorrentSwifter/Torrent.cs
+++ TorrentSwifter/Torrent.cs
@@ -81,6 +81,40 @@ namespace TorrentSwifter
                 return result;
             }
         }
+
+        private class TorrentFile
+        {
+            private readonly string path;
+            private readonly long size;
+            private readonly long offset;
+
+            public string Path
+            {
+                get { return path; }
+            }
+
+            public long Size
+            {
+                get { return size; }
+            }
+
+            public long Offset
+            {
+                get { return offset; }
+            }
+
+            public long EndOffset
+            {
+                get { return (offset + size); }
+            }
+
+            public TorrentFile(string path, long size, long offset)
+            {
+                this.path = path;
+                this.size = size;
+                this.offset = offset;
+            }
+        }
         #endregion
 
         #region Fields
@@ -90,6 +124,8 @@ namespace TorrentSwifter
         private readonly long totalSize;
 
         private Piece[] pieces = null;
+        private TorrentFile[] files = null;
+        private object[] fileWriteLocks = null;
         #endregion
 
         #region Properties
@@ -158,6 +194,9 @@ namespace TorrentSwifter
             this.totalSize = metaData.TotalSize;
 
             InitializePieces();
+            InitializeFiles();
+
+            // TODO: Initialize cached data, like integrity etc?
         }
         #endregion
 
@@ -179,6 +218,54 @@ namespace TorrentSwifter
                 int currentPieceSize = (isLastPiece ? lastPieceSize : pieceSize);
                 int blockCount = (((currentPieceSize - 1) / blockSize) + 1);
                 pieces[i] = new Piece(i, currentPieceSize, blockCount);
+            }
+        }
+
+        private void InitializeFiles()
+        {
+            var metaDataFiles = metaData.Files;
+            files = new TorrentFile[metaDataFiles.Length];
+            fileWriteLocks = new object[files.Length];
+
+            for (int i = 0; i < fileWriteLocks.Length; i++)
+            {
+                fileWriteLocks[i] = new object();
+            }
+
+            if (metaDataFiles.Length == 1)
+            {
+                var file = metaDataFiles[0];
+                files[0] = new TorrentFile(downloadPath, file.Size, 0L);
+
+                if (TorrentPreferences.AllocateFullFileSizes)
+                {
+                    IOHelper.CreateAllocatedFile(downloadPath, file.Size);
+                }
+                else
+                {
+                    IOHelper.CreateEmptyFile(downloadPath);
+                }
+            }
+            else if (metaDataFiles.Length > 1)
+            {
+                long currentFileOffset = 0L;
+
+                for (int i = 0; i < metaDataFiles.Length; i++)
+                {
+                    var metaDataFile = metaDataFiles[i];
+                    string filePath = IOHelper.GetTorrentFilePath(downloadPath, metaDataFile);
+                    files[i] = new TorrentFile(filePath, metaDataFile.Size, currentFileOffset);
+                    currentFileOffset += metaDataFile.Size;
+
+                    if (TorrentPreferences.AllocateFullFileSizes)
+                    {
+                        IOHelper.CreateAllocatedFile(filePath, metaDataFile.Size);
+                    }
+                    else
+                    {
+                        IOHelper.CreateEmptyFile(filePath);
+                    }
+                }
             }
         }
         #endregion
