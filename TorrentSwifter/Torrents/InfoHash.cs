67860818dfabfd19ec99442e7b12b699b7b2c02a
Mattias Edlund <matte.edlund@gmail.com>
2018-06-27 02:48:01 +0900

    Created an overload of the InfoHash constructor that takes an hexadecimal string rather than a byte array.

--- TorrentSwifter/Torrents/InfoHash.cs
+++ TorrentSwifter/Torrents/InfoHash.cs
@@ -33,6 +33,20 @@ namespace TorrentSwifter.Torrents
         }
 
         /// <summary>
+        /// Creates a new info hash.
+        /// </summary>
+        /// <param name="hex">The hash as an hexadecimal string.</param>
+        public InfoHash(string hex)
+        {
+            if (hex == null)
+                throw new ArgumentNullException("hex");
+            else if (hex.Length != 40)
+                throw new ArgumentException("The hash must be 20 bytes in size (40 hexadecimal characters).", "hex");
+
+            this.hash = HexHelper.HexToBytes(hex);
+        }
+
+        /// <summary>
         /// Returns if this info hash equals another object.
         /// </summary>
         /// <param name="obj">The other object.</param>
