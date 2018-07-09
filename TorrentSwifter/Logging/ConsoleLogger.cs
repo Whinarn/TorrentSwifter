2c98df9e269a5744f81581bcb5c9c5c99042f840
Mattias Edlund <matte.edlund@gmail.com>
2018-07-10 00:40:37 +0900

    Added support for console loggers.

---
+++ TorrentSwifter/Logging/ConsoleLogger.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace TorrentSwifter.Logging
+{
+    /// <summary>
+    /// A console logger.
+    /// </summary>
+    public sealed class ConsoleLogger : LoggerBase
+    {
+        private static readonly ConsoleColor[] LevelColors =
+            { ConsoleColor.White, ConsoleColor.DarkRed, ConsoleColor.Red, ConsoleColor.Yellow, ConsoleColor.White, ConsoleColor.Gray };
+        //    Off,                Fatal,                Error,            Warning,             Info,               Debug
+
+        private bool useColors = true;
+        private ConsoleColor defaultColor = ConsoleColor.White;
+
+        /// <summary>
+        /// Gets or sets if the console logger uses different colors depending on the log level.
+        /// </summary>
+        public bool UseColors
+        {
+            get { return useColors; }
+            set { useColors = value; }
+        }
+
+        /// <summary>
+        /// Creates a new console logger.
+        /// </summary>
+        public ConsoleLogger()
+            : this(true)
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new console logger.
+        /// </summary>
+        /// <param name="useColors">If this logger should use different colors depending on the log level.</param>
+        public ConsoleLogger(bool useColors)
+        {
+            this.useColors = useColors;
+            if (useColors)
+            {
+                try
+                {
+                    defaultColor = Console.ForegroundColor;
+                }
+                catch
+                {
+                    useColors = false;
+                    defaultColor = ConsoleColor.White;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Writes a log message to this logger.
+        /// </summary>
+        /// <param name="level">The log message level.</param>
+        /// <param name="message">The log message text.</param>
+        public override void Write(LogLevel level, string message)
+        {
+            if (useColors)
+            {
+                var levelColor = LevelColors[(int)level & LevelColors.Length];
+                Console.ForegroundColor = levelColor;
+            }
+            Console.Write(message);
+            if (useColors)
+            {
+                Console.ForegroundColor = defaultColor;
+            }
+        }
+
+        /// <summary>
+        /// Writes a log message to this logger including a trailing line-ending.
+        /// </summary>
+        /// <param name="level">The log message level.</param>
+        /// <param name="message">The log message text.</param>
+        public override void WriteLine(LogLevel level, string message)
+        {
+            if (useColors)
+            {
+                var levelColor = LevelColors[(int)level & LevelColors.Length];
+                Console.ForegroundColor = levelColor;
+            }
+            Console.WriteLine(message);
+            if (useColors)
+            {
+                Console.ForegroundColor = defaultColor;
+            }
+        }
+    }
+}
---
+++ TorrentSwifter/Logging/LoggerBase.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace TorrentSwifter.Logging
+{
+    /// <summary>
+    /// A logger base that includes some basic functionality for loggers.
+    /// </summary>
+    public abstract class LoggerBase : ILogger
+    {
+        private static readonly string[] LevelPrefixes = { "", "[FATAL] ", "[ERROR] ", "[WARNING] ", "[INFO] ", "[DEBUG] " };
+
+        /// <summary>
+        /// If timestamps should be prepended to all log messages.
+        /// </summary>
+        protected bool prependTimestamp = false;
+        /// <summary>
+        /// If log levels should be prepended to all log messages.
+        /// </summary>
+        protected bool prependLevel = true;
+
+        /// <summary>
+        /// Gets if timestamps should be prepended to all log messages.
+        /// </summary>
+        public bool PrependTimestamp
+        {
+            get { return prependTimestamp; }
+            set { prependTimestamp = value; }
+        }
+
+        /// <summary>
+        /// Gets if log levels should be prepended to all log messages.
+        /// </summary>
+        public bool PrependLevel
+        {
+            get { return prependLevel; }
+            set { prependLevel = value; }
+        }
+
+        /// <summary>
+        /// Creates a new logger base.
+        /// </summary>
+        public LoggerBase()
+        {
+
+        }
+
+        /// <summary>
+        /// The finalizer.
+        /// </summary>
+        ~LoggerBase()
+        {
+            Dispose(false);
+        }
+
+        /// <summary>
+        /// Disposes of this logger.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Disposes of this logger.
+        /// </summary>
+        /// <param name="disposing">If disposing, otherwise finalizing.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+
+        }
+
+        /// <summary>
+        /// Writes the log prefixes ahead of a message.
+        /// </summary>
+        /// <param name="level">The log message level.</param>
+        public virtual void WritePrefixes(LogLevel level)
+        {
+            if (prependTimestamp)
+            {
+                var date = DateTime.Now;
+                string datePrefixText = string.Format("[{0}] ", date.ToString("G"));
+                Write(level, datePrefixText);
+            }
+
+            if (prependLevel)
+            {
+                string prefixText = LevelPrefixes[(int)level & LevelPrefixes.Length];
+                Write(level, prefixText);
+            }
+        }
+
+        /// <summary>
+        /// Writes a log message to this logger.
+        /// </summary>
+        /// <param name="level">The log message level.</param>
+        /// <param name="message">The log message text.</param>
+        public abstract void Write(LogLevel level, string message);
+
+        /// <summary>
+        /// Writes a log message to this logger including a trailing line-ending.
+        /// </summary>
+        /// <param name="level">The log message level.</param>
+        /// <param name="message">The log message text.</param>
+        public abstract void WriteLine(LogLevel level, string message);
+    }
+}
