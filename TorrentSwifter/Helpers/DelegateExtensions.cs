dd9619911aa713dc7b3e2ab414738d9a6e3fb1ae
Mattias Edlund <matte.edlund@gmail.com>
2018-07-10 10:56:36 +0900

    Added support to safely invoke delegates.

---
+++ TorrentSwifter/Helpers/DelegateExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using TorrentSwifter.Logging;
+
+namespace TorrentSwifter
+{
+    internal static class DelegateExtensions
+    {
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void SafeInvoke(this EventHandler eventHandler, object sender, EventArgs args)
+        {
+            if (eventHandler != null)
+            {
+                try
+                {
+                    eventHandler.Invoke(sender, args);
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogErrorException(ex);
+                }
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void SafeInvoke<TEventArgs>(this EventHandler<TEventArgs> eventHandler, object sender, TEventArgs args)
+        {
+            if (eventHandler != null)
+            {
+                try
+                {
+                    eventHandler.Invoke(sender, args);
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogErrorException(ex);
+                }
+            }
+        }
+    }
+}
