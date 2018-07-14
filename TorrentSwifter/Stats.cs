4d50266eb8ce68fde8261d96b95e890fa2d0637d
Mattias Edlund <matte.edlund@gmail.com>
2018-07-15 01:28:45 +0900

    Added global download and upload rate limiters to the Stats class.

--- TorrentSwifter/Stats.cs
+++ TorrentSwifter/Stats.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading;
+using TorrentSwifter.Preferences;
 using TorrentSwifter.Torrents;
+using TorrentSwifter.Torrents.RateLimiter;
 
 namespace TorrentSwifter
 {
@@ -15,6 +17,9 @@ namespace TorrentSwifter
 
         private static RateMeasurer downloadRate = new RateMeasurer();
         private static RateMeasurer uploadRate = new RateMeasurer();
+
+        internal static readonly BandwidthLimiter downloadRateLimiter;
+        internal static readonly BandwidthLimiter uploadRateLimiter;
         #endregion
 
         #region Properties
@@ -35,7 +40,7 @@ namespace TorrentSwifter
         }
 
         /// <summary>
-        /// Gets the download rate in bytes per second for all combined downloading of all active torrents.
+        /// Gets the average download rate in bytes per second for all combined downloading of all active torrents.
         /// </summary>
         public static long DownloadRate
         {
@@ -43,11 +48,21 @@ namespace TorrentSwifter
         }
 
         /// <summary>
-        /// Gets the upload rate in bytes per second for all combined uploading of all active torrents.
+        /// Gets the average upload rate in bytes per second for all combined uploading of all active torrents.
         /// </summary>
         public static long UploadRate
         {
             get { return uploadRate.AverageRate; }
+        }
+        #endregion
+
+        #region Static Initializer
+        static Stats()
+        {
+            long downloadBandwidthLimit = Prefs.Torrent.DownloadBandwidthLimit;
+            long uploadBandwidthLimit = Prefs.Torrent.UploadBandwidthLimit;
+            downloadRateLimiter = new BandwidthLimiter(downloadRate, downloadBandwidthLimit);
+            uploadRateLimiter = new BandwidthLimiter(uploadRate, uploadBandwidthLimit);
         }
         #endregion
 
@@ -68,6 +83,13 @@ namespace TorrentSwifter
         {
             downloadRate.Update();
             uploadRate.Update();
+
+            // Update the global rate limiters in case the limits have changed
+            // TODO: Can we do this bit smarter?
+            long downloadBandwidthLimit = Prefs.Torrent.DownloadBandwidthLimit;
+            long uploadBandwidthLimit = Prefs.Torrent.UploadBandwidthLimit;
+            downloadRateLimiter.RateLimit = downloadBandwidthLimit;
+            uploadRateLimiter.RateLimit = uploadBandwidthLimit;
         }
         #endregion
     }
