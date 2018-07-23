2ab4eaf3d76d4bc6c25d1fe5b4126d1b6029dbd6
Mattias Edlund <matte.edlund@gmail.com>
2018-07-23 22:38:49 +0900

    The cross-platform UI is now setup for the Windows application.

--- TorrentSwifter.Windows/App.xaml.cs
+++ TorrentSwifter.Windows/App.xaml.cs
@@ -1,17 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace TorrentSwifter.Windows
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            TorrentEngine.Initialize();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            TorrentEngine.Uninitialize();
+
+            base.OnExit(e);
+        }
     }
 }
--- TorrentSwifter.Windows/Properties/AssemblyInfo.cs
+++ TorrentSwifter.Windows/Properties/AssemblyInfo.cs
@@ -54,4 +54,3 @@ using System.Windows;
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
-
