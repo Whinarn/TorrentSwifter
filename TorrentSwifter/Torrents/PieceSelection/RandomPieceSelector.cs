325b5e10dc4b2156170416cf0d4c3eba362390b1
Mattias Edlund <matte.edlund@gmail.com>
2018-07-24 12:50:18 +0900

    Implemented a random piece selector.

---
+++ TorrentSwifter/Torrents/PieceSelection/RandomPieceSelector.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TorrentSwifter.Helpers;
+
+namespace TorrentSwifter.Torrents.PieceSelection
+{
+    /// <summary>
+    /// A piece selector that randomly selects pieces for download.
+    /// </summary>
+    public sealed class RandomPieceSelector : IPieceSelector
+    {
+        private const double PieceImportanceNoise = 0.05;
+
+        /// <summary>
+        /// Returns the ranked pieces for downloading from this piece selector.
+        /// Note that this should not return already downloaded and verified pieces.
+        /// </summary>
+        /// <param name="torrent">The torrent.</param>
+        /// <param name="pieces">The torrent pieces.</param>
+        /// <returns>The enumeration of pieces.</returns>
+        public IEnumerable<TorrentPiece> GetRankedPieces(Torrent torrent, TorrentPiece[] pieces)
+        {
+            return pieces.Where((piece) => !piece.IsVerified)
+                .OrderByDescending((piece) => piece.Importance + RandomHelper.NextDouble(PieceImportanceNoise));
+        }
+    }
+}
